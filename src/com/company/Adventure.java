09b17cb6be4f5164193f77bad7cdeba955b04f09
Mark <mark771e@stud.kea.dk>
2022-03-14 14:02:17 +0100

    Added - commands() in Adventure

--- src/com/company/Adventure.java
+++ src/com/company/Adventure.java
@@ -2,8 +2,9 @@ package com.company;
 
 public class Adventure {
 
-    public static void main(String[] args) {
-        /*
+    private boolean running = true;
+    private Room currentRoom;
+    /*
         e
         w
         s
@@ -13,12 +14,34 @@ public class Adventure {
         help
 
         * */
+
+    public static void main(String[] args) {
+        new Adventure().run();
     }
 
 
     public void run() {
+        while(running){
+
+        }
+    }
+
+    public void commands(String command) {
+        switch (command){
+            case "e" -> System.out.println();
+            case "w" -> System.out.println();
+            case "n" -> System.out.println();
+            case "s" -> System.out.println();
+            case "look" -> System.out.println();
+            case "help" -> System.out.println();
+            case "exit" -> System.out.println();
+        }
+
 
     }
+
+    // public void createAndConnectRooms()
+
 
 }
 
