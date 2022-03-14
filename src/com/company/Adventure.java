9f0964c6c428a43e1b4ff129b90e4a56af3d41b6
Mark <mark771e@stud.kea.dk>
2022-03-14 14:07:27 +0100

    Changed - fleshed out commands()

--- src/com/company/Adventure.java
+++ src/com/company/Adventure.java
@@ -28,13 +28,14 @@ public class Adventure {
 
     public void commands(String command) {
         switch (command) {
-            case "e" -> System.out.println();
-            case "w" -> System.out.println();
-            case "n" -> System.out.println();
-            case "s" -> System.out.println();
-            case "look" -> System.out.println();
-            case "help" -> System.out.println();
-            case "exit" -> System.out.println();
+            case "e" -> System.out.println("go east");
+            case "w" -> System.out.println("go west");
+            case "n" -> System.out.println("go north");
+            case "s" -> System.out.println("go south");
+            case "look" -> System.out.println("look");
+            case "help" -> System.out.println("help");
+            case "exit" -> System.out.println("exit");
+            default -> System.out.println("invalid command");
         }
 
 
@@ -42,14 +43,15 @@ public class Adventure {
 
     public void createAndConnectRooms() {
         Room room1 = new Room("Room1", "Room1 desc");
-        Room room2 = new Room("Room1", "Room1 desc");
-        Room room3 = new Room("Room1", "Room1 desc");
-        Room room4 = new Room("Room1", "Room1 desc");
-        Room room5 = new Room("Room1", "Room1 desc");
-        Room room6 = new Room("Room1", "Room1 desc");
-        Room room7 = new Room("Room1", "Room1 desc");
-        Room room8 = new Room("Room1", "Room1 desc");
-        Room room9 = new Room("Room1", "Room1 desc");
+        Room room2 = new Room("Room2", "Room2 desc");
+        Room room3 = new Room("Room3", "Room3 desc");
+        Room room4 = new Room("Room4", "Room4 desc");
+        Room room5 = new Room("Room5", "Room5 desc");
+        Room room6 = new Room("Room6", "Room6 desc");
+        Room room7 = new Room("Room7", "Room7 desc");
+        Room room8 = new Room("Room8", "Room8 desc");
+        Room room9 = new Room("Room9", "Room9 desc");
+        room1.setEast(room2);
     }
 
 
